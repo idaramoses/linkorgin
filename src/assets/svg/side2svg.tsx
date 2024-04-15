d372aec2a1c7ccf8995148f6af68cbae7a1a315a
Pgodwin1 <pgodwin287@gmail.com>
2024-04-15 22:58:31 +0100

    added other signup pages

---
+++ src/assets/svg/side2svg.tsx
@@ -0,0 +1,22 @@
+
+const Side2Svg = (props: object) => (
+  <svg
+    width={448}
+    height={686}
+    viewBox="0 0 448 686"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <rect
+      x={916.821}
+      y={786.844}
+      width={1039}
+      height={571}
+      rx={285.5}
+      transform="rotate(-162.301 916.821 786.844)"
+      fill="#FEF0E8"
+    />
+  </svg>
+);
+export default Side2Svg;
--- src/pages/SignUp/basicDetails.tsx
+++ src/pages/SignUp/basicDetails.tsx
@@ -2,14 +2,66 @@ import './signUp.css'
 import { Header } from "../../components/signup/signUpHeader"
 import { Button } from "../../components/signup/button"
 import SideSvg from '../../assets/svg/sideangle'
+import Side2Svg from '../../assets/svg/side2svg'
 
 
 const BasicDetails = () => {
-  return (
-    <div>
-        <Header />
-    </div>
-  )
+    return (
+        <div>
+            <Header />
+            <div className='container min-vh-50 d-flex bg-white justify-content-center align-items-center'>
+                <div>
+                    <span>Basic Immigration Details</span>
+                    <form action="">
+                        <div className='d-flex flex-column mb-3'>
+                            <label htmlFor="">Country of Origin</label>
+                            <select name="country" id="">
+                                <option value="volvo">USA</option>
+                                <option value="saab">South Africa</option>
+                                <option value="mercedes">Nigeria</option>
+                                <option value="audi">Japan</option>
+                            </select>
+                        </div>
+                        <div className='d-flex flex-column mb-3'>
+                            <label htmlFor="">Current Immigration Status</label>
+                            <select name="country" id="">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </div>
+                        <div className='d-flex flex-column mb-3'>
+                            <label htmlFor="">Date of immigration or expected date of arrival</label>
+                            <input type="text" placeholder='10-07-2026' />
+                        </div>
+                        <div className='d-flex flex-column mb-3'>
+                            <label htmlFor="">Visa Type</label>
+                            <select name="country" id="">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </div>
+                        <div className='d-flex flex-column'>
+                            <label htmlFor="">type of Status</label>
+                            <select name="country" id="">
+                                <option value="volvo">Volvo</option>
+                                <option value="saab">Saab</option>
+                                <option value="mercedes">Mercedes</option>
+                                <option value="audi">Audi</option>
+                            </select>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <div className="w-50 pt-5 bg-dark svg" >
+                <Side2Svg />
+            </div>
+
+        </div>
+    )
 }
 
 export default BasicDetails
